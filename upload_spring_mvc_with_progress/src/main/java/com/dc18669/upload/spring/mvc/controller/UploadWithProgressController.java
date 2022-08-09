8107fe304ec7cec5ae74de59bf17832f8ff7119a
孟德方 <85704791+endless-ops@users.noreply.github.com>
2022-08-10 00:31:41 +0800

    基于spring mvc的文件上传，带有进度条显示效果

--- upload_spring_mvc_with_progress/src/main/java/com/dc18669/upload/spring/mvc/controller/UploadWithProgressController.java
+++ upload_spring_mvc_with_progress/src/main/java/com/dc18669/upload/spring/mvc/controller/UploadWithProgressController.java
@@ -1,10 +1,15 @@
 package com.dc18669.upload.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UploadWithProgressController {
 
-
+    @RequestMapping("upload_with_progress_bar")
+    public String upload(MultipartFile file) {
+        return null;
+    }
 
 }
